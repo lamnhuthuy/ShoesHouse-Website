740f4c5b282a9e6b0b5de32dbaee9233b66e40c9
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-01-23 22:45:57 +0700

    Feat: create file to config entities.

---
+++ ShoesHouse.Data/EF/ShoesHouseDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoesHouse.Data.Configurations;
+using ShoesHouse.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Data.EF
+{
+    class ShoesHouseDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public ShoesHouseDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppRoleConfiguration())
+                        .ApplyConfiguration(new AppUserConfiguration())
+                        .ApplyConfiguration(new CartConfiguration())
+                        .ApplyConfiguration(new CategoryConfiguration())
+                        .ApplyConfiguration(new CommentConfiguration())
+                        .ApplyConfiguration(new OrderConfiguration())
+                        .ApplyConfiguration(new OrderDetailConfiguration())
+                        .ApplyConfiguration(new ProductConfiguration())
+                        .ApplyConfiguration(new ProductImageConfiguration());
+            modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims");
+            modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(x => new { x.UserId, x.RoleId });
+            modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("AppUserLogins").HasKey(x => x.UserId);
+            modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("AppRoleClaims");
+            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens").HasKey(x => x.UserId);
+
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
+    }
+}
