62265b83f3be64a1a6ecc895766f97b0f6bb3639
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-01-23 10:59:28 +0700

    Feat: create enum status

---
+++ ShoesHouse.Data/Enums/Status.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Data.Enums
+{
+
+    public enum Status
+    {
+        Pending,
+        Confirmed,
+        Shipping,
+        Canceled,
+        Completed
+    }
+
+}
