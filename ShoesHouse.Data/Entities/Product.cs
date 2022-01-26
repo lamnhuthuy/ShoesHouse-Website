27aa329a2b5fec970debd279a9771159f19a5ec5
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-01-26 22:32:22 +0700

    Fix: Change prop in Product entity

--- ShoesHouse.Data/Entities/Product.cs
+++ ShoesHouse.Data/Entities/Product.cs
@@ -11,13 +11,14 @@ namespace ShoesHouse.Data.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public decimal Price { get; set; }
+        public decimal? Price { get; set; }
         public decimal OriginalPrice { get; set; }
+        public int Size { get; set; }
         public int Stock { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime? DateModified { get; set; }
         public DateTime? DateDeleted { get; set; }
-
+        public string Note { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
 
