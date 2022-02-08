75204cdaa3eaef02b313b54c5f277950fe379703
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-08 22:36:24 +0700

    Feat: add ProductService to implement IProductService

---
+++ ShoesHouse.Application/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using ShoesHouse.Application.Interfaces;
+using ShoesHouse.ViewModels.Requests.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Application.Services
+{
+    class ProductService : IProductService
+    {
+
+
+        public Task<int> CreateAsync(ProductCreateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(ProductUpdateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
