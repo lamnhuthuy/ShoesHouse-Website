9d0748f695fb58a4e2fb9ad7166c06f91ccf1a9f
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-08 22:34:51 +0700

    Feat: add Interface ICategory

---
+++ ShoesHouse.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using ShoesHouse.ViewModels.Requests.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<int> CreateAsync(CategoryCreateRequest request);
+
+        Task<int> UpdateAsync(CategoryUpdateRequest request);
+
+        Task<int> DeleteAsync(int categoryId);
+
+
+    }
+}
