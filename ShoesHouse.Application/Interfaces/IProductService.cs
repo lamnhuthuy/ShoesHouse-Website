78763d04d54e09a5614e97e0db179bd7bf09c863
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-08 22:35:19 +0700

    Feat: add Interface IProductService

---
+++ ShoesHouse.Application/Interfaces/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoesHouse.ViewModels.Requests.Product;
+
+namespace ShoesHouse.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<int> CreateAsync(ProductCreateRequest request);
+
+        Task<int> UpdateAsync(ProductUpdateRequest request);
+
+        Task<int> DeleteAsync(int ProductId);
+
+    }
+}
