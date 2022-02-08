007c0d74015177856b8289b07a217e6b3adec58a
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-08 22:36:08 +0700

    Feat: add CategoryService to implement ICategoryService

---
+++ ShoesHouse.Application/Services/CategoryService.cs
@@ -0,0 +1,39 @@
+﻿using ShoesHouse.Application.Interfaces;
+using ShoesHouse.ViewModels.Requests.Category;
+using ShoesHouse.ViewModels.Requests.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Application.Services
+{
+    class CategoryService : ICategoryService
+    {
+        public Task<int> CreateAsync(ProductCreateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> CreateAsync(CategoryCreateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(ProductUpdateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(CategoryUpdateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
