140a1652d3e3f32b2092d3c5dec8ef55552a3f32
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-08 22:34:03 +0700

    Feat: add Product Request

---
+++ ShoesHouse.ViewModels/Requests/Product/ProductUpdateRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.ViewModels.Requests.Product
+{
+    public class ProductUpdateRequest
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Size { get; set; }
+        public int Stock { get; set; }
+    }
+}
