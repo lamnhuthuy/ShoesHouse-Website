197d0cbe5f0f7eb4b9210839a9a99a6418a5c707
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-09 22:54:25 +0700

    Feat: create constants class

---
+++ ShoesHouse.Utilities/Constants/SystemConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Utilities.Constants
+{
+    public class SystemConstants
+    {
+        public const string MAIN_CONNECTION_STRING = "ShoesHouseDb";
+    }
+}
