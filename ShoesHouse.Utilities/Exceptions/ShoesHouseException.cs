79d3943913cd50de4adfe53731c0439c98da25f0
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-17 23:24:49 +0700

    Feat: create class exception

---
+++ ShoesHouse.Utilities/Exceptions/ShoesHouseException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Utilities.Exceptions
+{
+    public class ShoesHouseException : Exception
+    {
+        public ShoesHouseException()
+        {
+        }
+
+        public ShoesHouseException(string message)
+            : base(message)
+        {
+        }
+
+        public ShoesHouseException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
