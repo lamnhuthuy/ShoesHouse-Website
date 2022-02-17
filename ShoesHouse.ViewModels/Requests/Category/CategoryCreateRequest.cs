0fd70398ef073c43d3a40a47a9caec979ee05531
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-17 23:25:45 +0700

    Fix: update model validation for categoryCreateRequest

--- ShoesHouse.ViewModels/Requests/Category/CategoryCreateRequest.cs
+++ ShoesHouse.ViewModels/Requests/Category/CategoryCreateRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,11 @@ namespace ShoesHouse.ViewModels.Requests.Category
 {
     public class CategoryCreateRequest
     {
+        [Required(ErrorMessage = "Name is required")]
+        [Display(Name = "Name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Description is required")]
+        [Display(Name = "Description")]
         public string Description { get; set; }
     }
 }
