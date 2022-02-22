a6f52edc111aa1cb875c0442bc92769ef9753aa7
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-22 22:05:46 +0700

    Feat: creat NavigationVc

---
+++ ShoesHouse.AdminApp/Controllers/Components/NavigationViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.AdminApp.Controllers.Components
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
