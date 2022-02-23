b3d8d5ba9a55d11e8db6fae6734e60fe23bcd57a
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-23 23:13:49 +0700

    Feat: create LoginController

---
+++ ShoesHouse.AdminApp/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoesHouse.ApiIntegration.InterfacesClient;
+using ShoesHouse.ViewModels.Requests.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.AdminApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IUserApiClient _userApiClient;
+
+        public LoginController(IUserApiClient userApiClient)
+        {
+            _userApiClient = userApiClient;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _userApiClient.Authenticate(request);
+            if (result == null)
+            {
+                return BadRequest("incorrect");
+            }
+            return Ok(new { token = result });
+        }
+    }
+}
