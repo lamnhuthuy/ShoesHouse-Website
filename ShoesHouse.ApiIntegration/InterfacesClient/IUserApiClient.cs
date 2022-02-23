89adec7d63113004e133759d4d96c8d07a6eae72
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-02-23 23:09:05 +0700

    Feat: create ApiIntegration to call API

---
+++ ShoesHouse.ApiIntegration/InterfacesClient/IUserApiClient.cs
@@ -0,0 +1,15 @@
+﻿using ShoesHouse.ViewModels.Common;
+using ShoesHouse.ViewModels.Requests.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.ApiIntegration.InterfacesClient
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
---
+++ ShoesHouse.ApiIntegration/ServicesClient/UserApiClient.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using ShoesHouse.ApiIntegration.InterfacesClient;
+using ShoesHouse.Utilities.Constants;
+using ShoesHouse.ViewModels.Common;
+using ShoesHouse.ViewModels.Requests.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.ApiIntegration.ServicesClient
+{
+    public class UserApiClient : IUserApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _config;
+
+        public UserApiClient(IHttpClientFactory httpClientFactory, IConfiguration config)
+        {
+            _httpClientFactory = httpClientFactory;
+            _config = config;
+        }
+
+        public async Task<string> Authenticate(LoginRequest request)
+        {
+            var json = JsonConvert.SerializeObject(request);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(SystemConstants.MAIN_BASE_ADDRESS);
+            var response = await client.PostAsync("/api/Users/authenticate", httpContent);
+
+            var token = await response.Content.ReadAsStringAsync();
+
+            return token;
+
+        }
+    }
+}
