4cec8bf48388c60286dc133750c86739d24fd175
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-03-01 23:31:55 +0700

    Fix: Re-Structure

--- ShoesHouse.AdminApp/Controllers/LoginController.cs
+++ ShoesHouse.AdminApp/Controllers/LoginController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
 using ShoesHouse.ApiIntegration.InterfacesClient;
+using ShoesHouse.Utilities.Constants;
 using ShoesHouse.ViewModels.Requests.System.Users;
 using System;
 using System.Collections.Generic;
@@ -22,6 +24,7 @@ namespace ShoesHouse.AdminApp.Controllers
     {
         private readonly IUserApiClient _userApiClient;
         private readonly IConfiguration _configuration;
+
 
         public LoginController(IUserApiClient userApiClient, IConfiguration configuration)
         {
@@ -40,17 +43,19 @@ namespace ShoesHouse.AdminApp.Controllers
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(ModelState);
+                return View(request);
             }
             var result = await _userApiClient.Authenticate(request);
             if (result.ResultObj == null)
             {
-                return BadRequest("incorrect");
+                ModelState.AddModelError("", result.Message);
+                return View();
             }
+            HttpContext.Session.SetString(SystemConstants.AppSettings.Token, result.ResultObj);
             var userPrincipal = this.ValidateToken(result.ResultObj);
             var authProperties = new AuthenticationProperties
             {
-                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                ExpiresUtc = DateTimeOffset.UtcNow.AddHours(1),
                 IsPersistent = false
             };
             await HttpContext.SignInAsync(
@@ -63,6 +68,7 @@ namespace ShoesHouse.AdminApp.Controllers
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.Session.Remove(SystemConstants.AppSettings.Token);
             return RedirectToAction("Index", "Home");
         }
         private ClaimsPrincipal ValidateToken(string jwtToken)
