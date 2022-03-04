8ae6efdf4d7a138917d49ccebd8705a65c9fa930
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-03-04 22:43:26 +0700

    Feat: add ViewModel

---
+++ ShoesHouse.ViewModels/ViewModels/ProductImageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.ViewModels.ViewModels
+{
+    public class ProductImageViewModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string FileName { get; set; }
+        public string Caption { get; set; }
+    }
+}
---
+++ ShoesHouse.ViewModels/ViewModels/ProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.ViewModels.ViewModels
+{
+    public class ProductViewModel
+    {
+        public List<ProductImageViewModel> ProductImages { get; set; }
+        public List<CommentViewModel> Comments { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Size { get; set; }
+        public int Stock { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime? DateModified { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
