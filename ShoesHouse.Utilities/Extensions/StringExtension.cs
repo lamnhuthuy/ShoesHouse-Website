0ae1eef4f56ec63dccb5c7db4ce2f9910bed70ee
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-03-04 22:38:41 +0700

    Feat: add Extension for string variable

---
+++ ShoesHouse.Utilities/Extensions/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Utilities.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ToVND(this decimal number)
+        {
+            var culture = System.Globalization.CultureInfo.GetCultureInfo("vi-VN");
+            return String.Format(culture, "{0:N0}", number) + " đ";
+        }
+    }
+}
