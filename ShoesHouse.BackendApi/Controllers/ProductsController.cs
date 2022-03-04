45e2b452da82a404bee93245f1c57802e9fbec8d
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-03-04 22:43:55 +0700

    Feat: creat Productcontrooler

---
+++ ShoesHouse.BackendApi/Controllers/ProductsController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoesHouse.Application.Interfaces;
+using ShoesHouse.ViewModels.Requests.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Get()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(products);
+        }
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> Create([FromForm] ProductCreateRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var productId = await _productService.CreateAsync(request);
+            return Ok(productId);
+        }
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            try
+            {
+                var result = await _productService.DeleteAsync(productId);
+                if (result == 0)
+                {
+                    return BadRequest(result);
+                }
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpGet("{productId}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetById(int productId)
+        {
+            try
+            {
+                var product = await _productService.GetByIdAsync(productId);
+                if (product == null)
+                {
+                    return NotFound($"Cannot find a cake with Id: {product}");
+                }
+
+                return Ok(product);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update([FromForm] ProductUpdateRequest request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var result = await _productService.UpdateAsync(request);
+
+                if (result == 0)
+                {
+                    return BadRequest();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+    }
+}
