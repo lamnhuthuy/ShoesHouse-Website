c82dc3fb0eab82a184e870a27673c05d52db3bc9
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-03-04 22:40:42 +0700

    Feat: add mappingprofiles

---
+++ ShoesHouse.BackendApi/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ShoesHouse.Data.Entities;
+using ShoesHouse.ViewModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.BackendApi
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+            CreateMap<ProductImage, ProductImageViewModel>();
+            CreateMap<Comment, CommentViewModel>().ForMember(des => des.UserName, act => act.MapFrom(src => src.User.Name));
+        }
+    }
+}
