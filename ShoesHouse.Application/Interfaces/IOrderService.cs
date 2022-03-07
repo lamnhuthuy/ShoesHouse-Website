5f7d1bee65452642e191313b4cf1f7b5826909bd
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-03-07 23:06:36 +0700

    feat: Update all

---
+++ ShoesHouse.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using ShoesHouse.ViewModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Application.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<List<OrderViewModel>> GetAllAsync();
+    }
+}
---
+++ ShoesHouse.Application/Services/OrderService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoesHouse.Application.Interfaces;
+using ShoesHouse.Data.EF;
+using ShoesHouse.ViewModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.Application.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ShoesHouseDbContext _context;
+
+        public OrderService(ShoesHouseDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<OrderViewModel>> GetAllAsync()
+        {
+            return await _context.Orders.Include(x => x.User).Select(order => new OrderViewModel()
+            {
+                Id = order.Id,
+                UserName = order.User.Name,
+                Status = order.Status,
+                DeliveryDate = order.DeliveryDate,
+                DateCreated = order.DateCreated,
+                Total = order.Total,
+
+            }).ToListAsync();
+
+
+        }
+    }
+}
