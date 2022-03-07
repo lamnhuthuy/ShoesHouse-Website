ce630871a447aa0c62803e6d2308327494361060
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-03-07 23:05:30 +0700

    Feat: create order clientApiInterface  and  order clientApi

---
+++ ShoesHouse.ApiIntegration/InterfacesClient/IOrderApiClient.cs
@@ -0,0 +1,14 @@
+﻿using ShoesHouse.ViewModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.ApiIntegration.InterfacesClient
+{
+    public interface IOrderApiClient
+    {
+        Task<List<OrderViewModel>> GetAllAsync();
+    }
+}
---
+++ ShoesHouse.ApiIntegration/ServicesClient/OrderApiClient.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using ShoesHouse.ApiIntegration.InterfacesClient;
+using ShoesHouse.Application.Interfaces;
+using ShoesHouse.ViewModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesHouse.ApiIntegration.ServicesClient
+{
+    public class OrderApiClient : IOrderApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _config;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public OrderApiClient(IHttpClientFactory httpClientFactory, IConfiguration config, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClientFactory = httpClientFactory;
+            _config = config;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<List<OrderViewModel>> GetAllAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_config["BaseAddress"]);
+            var response = await client.GetAsync("/api/Orders");
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<List<OrderViewModel>>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
