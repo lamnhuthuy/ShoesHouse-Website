ef856f784c254a727423d42423ca3be3d94fb067
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2022-03-08 23:22:10 +0700

    Feat: create OrderBackEnd Api

--- ShoesHouse.BackendApi/Controllers/OrdersController.cs
+++ ShoesHouse.BackendApi/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ShoesHouse.Application.Interfaces;
+using ShoesHouse.ViewModels.Requests.Order;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace ShoesHouse.BackendApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class OrdersController : ControllerBase
     {
         private readonly IOrderService _orderService;
@@ -26,6 +28,31 @@ namespace ShoesHouse.BackendApi.Controllers
         {
             var orders = await _orderService.GetAllAsync();
             return Ok(orders);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateStatusAsync(UpdateStatusRequest request)
+        {
+            var result = await _orderService.UpdateStatusAsync(request);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] OrderCreateRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var orderId = await _orderService.CreateOrderAsync(request);
+            if (orderId == 0)
+            {
+                return BadRequest();
+            }
+            var data = await _categoryService.GetByIdAsync(categoryId);
+            if (data == null)
+            {
+                return NotFound($"Cannot find a order with Id: {categoryId}");
+            }
+            return Ok(data);
         }
     }
 }
